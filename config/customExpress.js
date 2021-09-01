07f7dd7e2c6b077dbd4a8b72b57435206101e3dc
Rodrigo Neto <rodrigoneto.forseti@gmail.com>
2021-09-01 19:28:22 -0300

    Separando responsabilidade

---
+++ config/customExpress.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const consign = require('consign')
+const app = express()
+
+
+
+module.exports = () => {
+    const app = express()
+
+    consign()
+        .include('controllers')
+        .into(app)
+
+    return app
+
+}
