f6b23c68ff3423e7b1bb6ea011d6b7872a71f3f3
Rodrigo <rodrigoneto.forseti@gmail.com>
2021-12-23 07:59:50 -0300

    Ajustando env do projeto

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2')
 const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user:'rodrigo',
-    password: '1803',
-    database: 'agenda-petshop'
+    user:'root',
+    password: 'senha',
+    database: 'agenda_petshop'
 })
 
 module.exports = conexao
